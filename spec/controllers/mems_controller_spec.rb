c4af37d14d531424b7c34eccec89cba79b66e76c
Marley Kanui <marley.kanui@gmail.com>
2020-03-05 03:39:00 +0000

    MEM added new action and rspec test

--- spec/controllers/mems_controller_spec.rb
+++ spec/controllers/mems_controller_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe MemsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "mems#new action" do 
+    it "should successfully show the add memory form" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end 
+  end 
+  
 end
