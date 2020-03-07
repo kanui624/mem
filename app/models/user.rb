d9ca82effa2d8bf0df985505b68427bd9d3710ee
Marley Kanui <marley.kanui@gmail.com>
2020-03-07 22:41:26 +0000

    MEM Styled Devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, presence: true, length: {maximum: 30, minimun: 3}
+  validates :last_name, presence: true, length: {maximum: 30, minimun: 3}
+
   has_many :memories
   # has_many :photos 
 end
